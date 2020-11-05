22c15ea0dbdc434e7214edd8470d393f99cbdc48
Stepan Smirnov <stechkin1986@gmail.com>
2020-11-05 15:23:09 +0300

    modified:   src/BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,6 @@ public class BonusMilesService {
     public int calculate(int cost) {
         int priceBonus = 20;
         int miles = cost / priceBonus;
-        System.out.println(miles);
         return miles;
     }
 }
